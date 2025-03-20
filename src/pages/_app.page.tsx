86c4339d07eed9435909957bf83f7d213c0191e4
Abdul <shekhaabdulkadir@gmail.com>
2025-03-20 20:20:43 +0530

    add meta title and description

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,10 +1,18 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Yt data migrator</title>
+        <meta
+          name="description"
+          content="Transfer your subscriptions and playlists from one youtube account to another"
+        />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer position="bottom-right" />
     </>
